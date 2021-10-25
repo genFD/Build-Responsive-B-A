493fafc9129f37ec12e1e84cbaefb4ecba14a629
hermkan <hermann.kanga.dev@gmail.com>
2021-10-25 19:45:33 -0400

    created markup for contact section + completed styles

--- script.js
+++ script.js
@@ -27,25 +27,3 @@ playBtn.addEventListener('click', () => {
   playPause();
 });
 video.addEventListener('timeUpdate');
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
